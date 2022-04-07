4a2f25221c3e45754bc8c9b379b6604090c8bee5
Chris Davis <contact@nthstate.com>
2022-04-07 15:28:35 +0100

    Create container, list to nested strcuture

--- Plugins/SourceGenerator/SourceGenerator.swift
+++ Plugins/SourceGenerator/SourceGenerator.swift
@@ -13,34 +13,41 @@ struct SourceGenerator: BuildToolPlugin {
     //      return []
     //    }
     //
-    let resourcesDirectoryPath = context.pluginWorkDirectory
-      .appending(subpath: target.name)
+    //    let resourcesDirectoryPath = context.pluginWorkDirectory
+    //      .appending(subpath: target.name)
     //.appending(subpath: "Resources")
     //    print("resourcesDirectoryPath file: \(resourcesDirectoryPath)")
 
     let input = context.package.directory.appending("input.csv")
+    let lines = fileToArray(at: input.string)
+    let d = linesToDict(lines: lines)
+
+    var outputStr: String = ""
+    writeFileRecurive(container: d, str: &outputStr, space: 0)
+
+
 
     let outputPath = context.pluginWorkDirectory.appending("GeneratedSources")
 
     do {
-      try  FileManager.default.createDirectory(at: URL(fileURLWithPath: outputPath.string), withIntermediateDirectories: true, attributes: nil)
+      try FileManager.default.createDirectory(at: URL(fileURLWithPath: outputPath.string), withIntermediateDirectories: true, attributes: nil)
     } catch let error {
       print(error)
     }
 
     //let sourceFolder = context.package.directory//.appending(["Sources", "GeneralAccessibility"])
 
-//    print("SourceGenerator running")
-//    print("Input file: \(input)")
-//    print("pluginDirectory: \(pluginDirectory)")
-//    print("packageDirectory: \(context.package.directory)")
+    //    print("SourceGenerator running")
+    print("Input file: \(input)")
+    //    print("pluginDirectory: \(pluginDirectory)")
+    //    print("packageDirectory: \(context.package.directory)")
 
-    let src = outputPath.appending("foo.swift")
+    let src = outputPath.appending("General.swift")
     //print("write: \(file)")
 
-   // let out = resourcesDirectoryPath.appending("foo.swift")
+    // let out = resourcesDirectoryPath.appending("foo.swift")
 
-    let data = "import Foundation \n public struct Foo { \n public init() {} \n}".data(using: .utf8)
+    let data = outputStr.data(using: .utf8)
 
     writeFile(to: src.string, with: data)
 
@@ -51,8 +58,8 @@ struct SourceGenerator: BuildToolPlugin {
     return [.prebuildCommand(displayName: "Test",
                              executable: .init("/bin/cp"),
                              arguments: [
-//                              src.string,
-//                              outputPath.string
+                              //                                                            src.string,
+                              //                                                            outputPath.string
                              ],
                              outputFilesDirectory: outputPath)]
   }
@@ -63,4 +70,104 @@ struct SourceGenerator: BuildToolPlugin {
     //print("written: \(written)")
   }
 
+  func fileToArray(at: String) -> [String] {
+
+    guard let inputString = try? String(contentsOfFile: at) else {
+      return []
+    }
+
+    let parts = inputString.components(separatedBy: CharacterSet(charactersIn: "\n"))
+    let trimmed = parts.compactMap({ $0.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) })
+
+    return trimmed
+  }
+
+  func linesToDict(lines: [String]) -> Container {
+
+    var container = Container(name: "General", children: [: ])
+
+    for line in lines {
+
+      let parts = line.components(separatedBy: CharacterSet(charactersIn: ","))
+
+      let key = parts[0]
+      //print("key: \(key)")
+
+      let c: Container
+      if container.children[key] != nil {
+        //print("parent gound: \(key)")
+        c = container.children[key]!
+        addPart(parts: Array(parts[1...]), parent: c)
+      } else {
+        c = Container(name: key, children: [: ])
+        addPart(parts: Array(parts[1...]), parent: c)
+        container.children[key] = c
+      }
+
+
+
+      //print("c: \(c)")
+
+
+    }
+
+    //print("Container: \(container)")
+
+    return container
+  }
+
+  func addPart(parts: [String], parent: Container) {
+
+    //print("Add parts: \(parts)")
+    
+    guard parts.count >= 1 else {
+      return
+    }
+
+    let key = parts[0]
+    //print("subbbb: \(key)")
+
+    let c: Container
+    if parent.children[key] != nil {
+      //print("sub: \(key)")
+      c = parent.children[key]!
+    } else {
+      c = Container(name: key, children: [: ])
+      parent.children[key] = c
+    }
+
+    //
+
+    addPart(parts: Array(parts[1...]), parent: c)
+  }
+
+  func writeFileRecurive(container: Container, str: inout String, space: Int) {
+
+    let spaces = String(repeating: " ", count: space)
+    str += "\(spaces)public struct \(container.name) {\n"
+
+    for item in container.children {
+      print("Child: \(item.key)")
+      writeFileRecurive(container: item.value, str: &str, space: space + 2)
+    }
+
+    str += "\(spaces)}\n"
+  }
+
+}
+
+class Container {
+  let name: String
+  var children: [String: Container]
+
+  init(name: String, children: [String: Container]) {
+    self.name = name
+    self.children = children
+  }
+}
+
+extension Container: CustomStringConvertible {
+  var description: String {
+    "Name: \(name), Child Count: \(children.count) \(children)"
+  }
 }
